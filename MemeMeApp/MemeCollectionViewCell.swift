a82ec824a5e29e30de85a85eb4d5abf95fb36e4f
Mikaila Akeredolu <makerofappsdotcom@gmail.com>
2015-04-14 20:04:54 -0400

    Committed by Udacity

---
+++ MemeMeApp/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMeApp
+//
+//  Created by Mikaila Akeredolu on 4/7/15.
+//  Copyright (c) 2015 MakerOfAppsDotCom. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var collectionVCImageOutlet: UIImageView!
+    
+}
